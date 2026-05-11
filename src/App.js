ad5bb90fc2a5ffb10e6dcda70a54935dbb622c07
Sunil Sahu <sahusunil90@gmail.com>
2026-05-11 19:46:42 +0530

    using props added multiple products

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import './App.css';
 
+
+function ProductCard(props){
+  return (
+    <div className='product-card'>
+      <img src={props.image} alt={props.name}/>
+      <h3>{props.name}</h3>
+      <p>{props.description}</p>
+      <span className="price">${props.price}</span>
+      <button onClick={()=> alert(`Added ${props.name} to cart`)}>Add to Cart</button>
+    </div>
+  )
+}
 function App() {
 
   const productName = "gaming laptop pro";
@@ -11,15 +23,24 @@ function App() {
   return (
     <div className="App">
       <h1>Product showcase</h1>
-      <div className='product-card' id={productId}>
-        <img src={productImage} alt={productName}
-          title={`Click to view ${productName} details`}/>
-        <h3>{productName}</h3>
-        <p>{productDescription}</p>
-        <span className='price'>${productPrice}</span>
-        <button>Add to Cart</button>
-      </div>
-      
+      <ProductCard
+        name="gaming laptop pro"
+        description="high performance laptop for gaming"
+        price={1299}
+        image="/images/lapotp.jpg"/>
+
+
+        <ProductCard
+        name="gaming laptop pro"
+        description="high performance laptop for gaming"
+        price={1299}
+        image="/images/lapotp.jpg"/>
+
+        <ProductCard
+        name="gaming laptop pro"
+        description="high performance laptop for gaming"
+        price={1299}
+        image="/images/lapotp.jpg"/>
     </div>
   );
 }
