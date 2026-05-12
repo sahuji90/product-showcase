407a237a06ff698f899dbcf4fc2b49e3d00a85d6
Sunil Sahu <sahusunil90@gmail.com>
2026-05-12 09:58:02 +0530

    fixed props deconstructuring

--- src/App.js
+++ src/App.js
@@ -1,42 +1,23 @@
 import './App.css';
 
-
-function ProductCard(props){
+  //below is example of destructuring
+function ProductCard({name, image, description, price, isOnSale=false,salePrice=price} ){
   return (
     <div className='product-card'>
-      <img src={props.image} alt={props.name}/>
-      <h3>{props.name}</h3>
-      <p>{props.description}</p>
-      <span className="price">${props.price}</span>
-      <button onClick={()=> alert(`Added ${props.name} to cart`)}>Add to Cart</button>
+      <img src={image} alt={name}/>
+      <h3>{name}</h3>
+      <p>{description}</p>
+      <span className="price">${price}</span>
+      <button onClick={()=> alert(`Added ${name} to cart`)}>Add to Cart</button>
     </div>
   )
 }
 function App() {
 
-  const productName = "gaming laptop pro";
-  const productPrice = 1299;
-  const productDescription = "High - performance laptop";
-  const isOnSale = true;
-  const productImage = "/images/laptop.jpg";
-  const productId = "laptop-001";
   return (
     <div className="App">
       <h1>Product showcase</h1>
       <ProductCard
-        name="gaming laptop pro"
-        description="high performance laptop for gaming"
-        price={1299}
-        image="/images/lapotp.jpg"/>
-
-
-        <ProductCard
-        name="gaming laptop pro"
-        description="high performance laptop for gaming"
-        price={1299}
-        image="/images/lapotp.jpg"/>
-
-        <ProductCard
         name="gaming laptop pro"
         description="high performance laptop for gaming"
         price={1299}
